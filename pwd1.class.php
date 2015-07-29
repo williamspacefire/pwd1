98a7689637415b911e55949fe4c1008a96c23e44
William Spacefire <william@spacefire.com.br>
2015-07-29 20:45:13 -0300

    GNU 2.0 adicionado ao arquivo
    foi adicionado a licença GNU 2.0

--- pwd1.class.php
+++ pwd1.class.php
@@ -1,14 +1,28 @@
 <?php
 /**
-* © Spacefire, inc.
-*----------------------------------------------------------
-*| Classe simples de Salt para deixar senhas mais seguras |
-*----------------------------------------------------------
+* 
+*    Classe simples de Salt para deixar senhas mais seguras
+* 
+*    Copyright (C) 2015  Spacefire, inc.
 *
-* @copyright	Spacefire, inc.
-* @author 	William Spacefire
-* @category 	Criptografia
-* @version	2.4
+*    This program is free software; you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation; either version 2 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License along
+*    with this program; if not, write to the Free Software Foundation, Inc.,
+*    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*
+* @copyright    Spacefire, inc.
+* @author       William Spacefire
+* @category     Criptografia
+* @version      2.4
 */
 class pwd1{
 	/**
